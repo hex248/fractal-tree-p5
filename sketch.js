49316f014113eddc69437b18f118da60b388f2ad
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2024-01-27 04:11:24 +0000

    Fractal tree!

--- sketch.js
+++ sketch.js
@@ -1,9 +1,33 @@
-// Your p5.js code goes here
+var depth = 10;
+var rotateAngle = 0;
+var slider;
+
 function setup() {
-    createCanvas(500, 500);
-    background(20);
+    createCanvas(700, 700);
+    colorMode(HSB, 100);
+    stroke(random(100), 80, 100);
+    slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 }
 
 function draw() {
-    // Add your drawing logic here
+    background(10);
+    rotateAngle = slider.value();
+    translate(width / 2, height);
+    branch(height / 4);
+}
+
+function branch(length) {
+    stroke(length / 1, 80, 100);
+    line(0, 0, 0, -length);
+    translate(0, -length);
+    if (length > 4) {
+        push();
+        rotate(rotateAngle);
+        branch(length * 0.7);
+        pop();
+        push();
+        rotate(-rotateAngle);
+        branch(length * 0.7);
+        pop();
+    }
 }
